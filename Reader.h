88102390810317410900e55e6e98832e640c49be
Brandon Runnels <brunnels@caltech.edu>
2015-02-02 02:51:40 -0800

    bugfix: used variables in included file

--- Reader.h
+++ Reader.h
@@ -381,6 +381,7 @@ private: // Private member functions
 	    string includeFileName = line.replace(line.find(include),include.size(),"");
 	    Reader includeReader(includeFileName, variableDelimiter, commentDelimiter,lineOverflowDelimiter);
 	    variables.insert(includeReader.variables.begin(),includeReader.variables.end());
+	    usedVariables.insert(includeReader.usedVariables.begin(),includeReader.usedVariables.end());
 	  }
 
 	// 3. Store all structs
